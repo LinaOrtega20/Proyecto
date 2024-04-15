f49086fd4f973e1f63151f107808aa9d168de79b
JulianBuitrago12 <162070592+JulianBuitrago12@users.noreply.github.com>
2024-04-14 23:25:17 -0500

    Notas

--- frontend/estructuras/area_derecha/area_der2.py
+++ frontend/estructuras/area_derecha/area_der2.py
@@ -5,27 +5,27 @@ from dash import html, dcc
 area_der2= dbc.Container([
     html.Div([
         html.Label('Velocidad (m/s):   '),
-        html.Label(id = 'respuestaV'),
+        html.Label(id = 'respuestaV'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante 
     ]),
 
     html.Div([
         html.Label('Caudal (m3/s):   '),
-        html.Label(id = 'respuestaQ'),
+        html.Label(id = 'respuestaQ'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
     ]),
 
     html.Div([
         html.Label('Pérdida por fricción (Hf):   '),
-        html.Label(id = 'respuestaHF'),
+        html.Label(id = 'respuestaHF'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
     ]),
 
     html.Div([
         html.Label('Pérdida local (Hl):   '),
-        html.Label(id = 'respuestaHL'),
+        html.Label(id = 'respuestaHL'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
     ]),
 
     html.Div([
         html.Label('Coeficiente de fricción (f):   '),
-        html.Label(id = 'respuestaF'),
+        html.Label(id = 'respuestaF'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
     ]),
 
 
