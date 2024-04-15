f0d62087e26c92d33e3540a87a8a85c1253d3a53
JulianBuitrago12 <162070592+JulianBuitrago12@users.noreply.github.com>
2024-04-14 23:26:54 -0500

    Notas area der 1

--- frontend/estructuras/area_derecha/area_der1.py
+++ frontend/estructuras/area_derecha/area_der1.py
@@ -5,12 +5,12 @@ from dash import html, dcc
 area_der1= dbc.Container([
     html.Div([
         html.Label('Función objetivo:   '),
-        html.Label(id = 'respuestaFO'),
+        html.Label(id = 'respuestaFO'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
     ]),
 
     html.Div([
         html.Label('Pérdida por fricción (hf):   '),
-        html.Label(id = 'respuestaHF'),
+        html.Label(id = 'respuestaHF'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
     ]),
     
 ])
