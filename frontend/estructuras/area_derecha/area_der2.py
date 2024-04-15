15c417698f1c3df790101e1e283457e5a2910007
LinaOrtega20 <lfoc20@gmail.com>
2024-04-14 23:33:23 -0500

    final

--- frontend/estructuras/area_derecha/area_der2.py
+++ frontend/estructuras/area_derecha/area_der2.py
@@ -5,27 +5,27 @@ from dash import html, dcc
 area_der2= dbc.Container([
     html.Div([
         html.Label('Velocidad (m/s):   '),
-        html.Label(id = 'respuestaV'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante 
+        html.Label(id = 'respuestaV'), #Este resultado va con formulas complejas, por lo tanto se haran con un backend más adelante 
     ]),
 
     html.Div([
         html.Label('Caudal (m3/s):   '),
-        html.Label(id = 'respuestaQ'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
+        html.Label(id = 'respuestaQ'), #Este resultado va con formulas complejas, por lo tanto se haran con un backend más adelante
     ]),
 
     html.Div([
         html.Label('Pérdida por fricción (Hf):   '),
-        html.Label(id = 'respuestaHF'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
+        html.Label(id = 'respuestaHF'), #Este resultado va con formulas complejas, por lo tanto se haran con un backend más adelante
     ]),
 
     html.Div([
         html.Label('Pérdida local (Hl):   '),
-        html.Label(id = 'respuestaHL'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
+        html.Label(id = 'respuestaHL'), #Este resultado va con formulas complejas, por lo tanto se haran con un backend más adelante
     ]),
 
     html.Div([
         html.Label('Coeficiente de fricción (f):   '),
-        html.Label(id = 'respuestaF'), #Este resultado va con formulas complejas, por lo tanto se haran con un bakend más adelante
+        html.Label(id = 'respuestaF'), #Este resultado va con formulas complejas, por lo tanto se haran con un backend más adelante
     ]),
 
 
