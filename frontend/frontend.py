eb5d3eba474a276c4cdb27a5597a203da9264c35
JulianBuitrago12 <162070592+JulianBuitrago12@users.noreply.github.com>
2024-04-21 22:45:58 -0500

    Edit final

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -11,8 +11,6 @@ layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H2("DISEÑO DE TUBERÍAS", style={'font-weight': 'bold'}), md=12, style={'background-color': '#7CCD7C'}),
         dbc.Col(encabezado,md=12,style={'background-color':'#7CCD7C'}),
-        dbc.Col("Modelo",md=12,style={'background-color':'#7CCD7C'}),
-        # se insertara la imagen del modelo 
         dbc.Col(izquierda,md=6,style={'background-color':'#7CCD7C'}),
         dbc.Col(derecha,md=6,style={'background-color':'#7CCD7C'}),
         dbc.Col(formulas,md=12,style={'background-color':'#7CCD7C'}), 
